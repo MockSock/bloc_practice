d414e46fa17caed0b539f4b93156cf3b2eceea17
madden20202 <business.mocksock@gmail.com>
2022-08-03 15:33:26 -0500

    add imports and notes

---
+++ lib/calculation_state.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+import 'calculation_model.dart';
+
+// Since it is just one state to worry about, this is easy
+// this one state to worry about is just what is being calculated
+
