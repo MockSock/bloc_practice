69fbb4e42480b8d68d78ac27a20bb0821f3b4a1d
madden20202 <business.mocksock@gmail.com>
2022-08-03 15:06:27 -0500

    create NumberPressedClass

--- lib/calculation_model.dart
+++ lib/calculation_model.dart
@@ -4,3 +4,12 @@ import 'package:meta/meta.dart';
 abstract class CalculationEvent extends Equatable {
   const CalculationEvent();
 }
+
+class NumberPressed extends CalculationEvent {
+  final int number;
+
+  const NumberPressed({required this.number});
+
+  @override
+  List<Object> get props => [number];
+}
