17b0cea8f9e3d07184c7fe9bafcaf514da3a9567
madden20202 <business.mocksock@gmail.com>
2022-08-03 15:41:29 -0500

    add variables

---
+++ lib/calculation_model.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class CalculationModel extends Equatable {
+  CalculationModel({});
+
+  final int? firstOperand;
+  final String? operator;
+  final int? secondOperand;
+  final int? result;
+  
+}
